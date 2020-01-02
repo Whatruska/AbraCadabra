a88b6eeb6e3f227b99e4d89007a9e1cec2cea694
Whatruska <vitdub@inbox.ru>
2020-01-03 01:23:45 +0600

    one more little fix

--- src/Ticket_2/Arrays.java
+++ src/Ticket_2/Arrays.java
@@ -2,20 +2,26 @@ package Ticket_2;
 
 public class Arrays {
     public static void main(String[] args) {
-        String[] strings = new String[]{};
+        String[] strings = new String[]{"itiskfu", "aabcde", "aabcde"};
+        boolean isOK = true;
         for (int i = 0; i < strings.length - 1; i++){
             String str1 = strings[i];
             String str2 = strings[i + 1];
             boolean[] map = getLetterMap(str1);
+
             for (int j = 0; j < str2.length(); j++){
                 char ch = str2.charAt(j);
-                int index = ch - 'a' - 1;
+                int index = ch - 'a';
                 if (map[index]){
                     System.out.println("Нашли не подходящую строку");
+                    isOK = false;
+                    break;
                 }
             }
         }
-        System.out.println("Все ок");
+        if (isOK) {
+            System.out.println("Все ок");
+        }
     }
 
     public static boolean[] getLetterMap(String str){
@@ -23,7 +29,7 @@ public class Arrays {
         str = str.toLowerCase();
         for (int i = 0; i < str.length(); i++){
             char ch = str.charAt(i);
-            int index = ch - 'a' - 1;
+            int index = ch - 'a';
             map[index] = true;
         }
         return map;
